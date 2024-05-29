3683f55cd5162b8d4f94b8867e9b3cfb7c0d9c8b
Tyfer7 <stevetyfer73@gmail.com>
2024-05-29 19:08:42 +0300

    Add files via upload

---
+++ word.c
@@ -0,0 +1,20 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    printf("Hey \n");
+    printf("New skill unlocked \n");
+   printf("We talk after this \n");
+    printf("I wish i jump to conclusion\n");
+
+    return 0;
+}
+/tmp/hQX1uNwrZv.o
+Hey 
+New skill unlocked 
+We talk after this 
+I wish i jump to conclusion
+
+
+=== Code Execution Successful ===
